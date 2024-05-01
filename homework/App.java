11a1bced05c5cc41efd11ec76f62c8c5fbf02311
erictslee <ts5452@naver.com>
2024-05-01 17:46:36 +0900

    개인별 숙제 Level1_5

--- homework/App.java
+++ homework/App.java
@@ -1,5 +1,6 @@
 package homework;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.awt.SystemColor.text;
@@ -7,49 +8,75 @@ import static java.awt.SystemColor.text;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] intArray= new int[10];
+        int currentIndex=0; //현재 배열에 저장된 요소의 개수
 
-        while(true){
+        while(true) {
 
             System.out.println("첫 번째 숫자를 입력하세요 :");
-            Integer input1=sc.nextInt();
+            Integer input1 = sc.nextInt();
 
             System.out.println("두 번째 숫자를 입력하세요 :");
-            Integer input2=sc.nextInt();
+            Integer input2 = sc.nextInt();
 
 
             System.out.println("사칙연산 기호를 입력하세요 :");
-            String input3=sc.next();
+            String input3 = sc.next();
+
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String input4=sc.next();
+            String input4 = sc.next();
 
-            if(input4.equals("exit")){
+            if (input4.equals("exit")) {
                 break;
             }
 
-            int answer=0;
+            int answer = 0;
 
-            switch (input3){
+            switch (input3) {
 
                 case "+":
-                    answer=input1+input2;
-                    System.out.println("결과 : "+answer);
+                    answer = input1 + input2;
+                    if(currentIndex < intArray.length) {
+                        intArray[currentIndex] = answer;
+                    }else {
+
+                        System.out.println("배열이 가득 찼습니다.");
+                    }
                     break;
 
                 case "-":
-                    answer=input1-input2;
-                    System.out.println("결과 : "+answer);
-                    break;
+                    answer = input1 - input2;
+                    if(currentIndex < intArray.length) {
+                        intArray[currentIndex] = answer;
+                    }else {
+
+                        System.out.println("배열이 가득 찼습니다.");
+                    }
+                     break;
 
                 case "*":
-                    answer=input1*input2;
-                    System.out.println("결과 : "+answer);
+                    answer = input1 * input2;
+                    if(currentIndex < intArray.length) {
+                        intArray[currentIndex] = answer;
+                    }else {
+
+                        System.out.println("배열이 가득 찼습니다.");
+                    }
                     break;
 
                 case "/":
-                    answer=input1/input2;
-                    System.out.println("결과 : "+answer);
+                    answer = input1 / input2;
+                    if(currentIndex < intArray.length) {
+                        intArray[currentIndex] = answer;
+                    }else {
+
+                        System.out.println("배열이 가득 찼습니다.");
+                    }
                     break;
+
+
+
             }
 
         }
