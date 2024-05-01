5085d988a0328c3d075295df08f159a4bda285cb
erictslee <ts5452@naver.com>
2024-05-01 16:30:35 +0900

    개인별 숙제 Level1_4

--- homework/App.java
+++ homework/App.java
@@ -2,39 +2,60 @@ package homework;
 
 import java.util.Scanner;
 
+import static java.awt.SystemColor.text;
+
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: "); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int input1 = Integer.parseInt(sc.nextLine());
+        while(true){
 
-        System.out.print("두 번째 숫자를 입력하세요: "); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int input2 = Integer.parseInt(sc.nextLine());
+            System.out.println("첫 번째 숫자를 입력하세요 :");
+            Integer input1=sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: "); // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다. dddd
-        String input3= sc.nextLine();
+            System.out.println("두 번째 숫자를 입력하세요 :");
+            Integer input2=sc.nextInt();
 
-        int result = 0;
 
-            if (input3.equals("+")) {
+            System.out.println("사칙연산 기호를 입력하세요 :");
+            String input3=sc.next();
 
-                result=input1+input2;
-                System.out.println("결과 : "+result);
-            } else if (input3.equals("-")) {
-                result=input1-input2;
-                System.out.println("결과 : "+result);
-            } else if (input3.equals("*")) {
-                result=input1*input2;
-                System.out.println("결과 : "+result);
-            } else if (input3.equals("/")) {
-                result=input1/input2;
-                System.out.println("결과 : "+result);
-            } else {
-                System.out.println("잘못된 입력입니다.");
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String input4=sc.next();
+
+            if(input4.equals("exit")){
+                break;
             }
 
+            int answer=0;
+
+            switch (input3){
+
+                case "+":
+                    answer=input1+input2;
+                    System.out.println("결과 : "+answer);
+                    break;
+
+                case "-":
+                    answer=input1-input2;
+                    System.out.println("결과 : "+answer);
+                    break;
+
+                case "*":
+                    answer=input1*input2;
+                    System.out.println("결과 : "+answer);
+                    break;
+
+                case "/":
+                    answer=input1/input2;
+                    System.out.println("결과 : "+answer);
+                    break;
+            }
+
+        }
 
 
     }
+
 }
+
