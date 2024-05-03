ad0b6fde8422d2b921a1f843526c721b8a16f0ba
erictslee <ts5452@naver.com>
2024-05-03 19:25:09 +0900

    개인별 숙제 Level1_7

--- homework/App.java
+++ homework/App.java
@@ -4,71 +4,49 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        Calculator calculator=new Calculator();
 
-        int result = 0;
-        List<Integer> results = new ArrayList<>();
+        int index=0;
+        while (true){
+
+        System.out.println("첫 번째 숫자를 입력하세요 : ");
+        int num1= sc.nextInt();
+
+        System.out.println("두 번째 숫자를 입력하세요 : ");
+        int num2= sc.nextInt();
+
+        System.out.println("사칙연산 기호를 입력하세요 : ");
+        Character operator= sc.next().charAt(0);
+
+        int result= calculator.calculate(num1, num2, operator);
+
+        calculator.getResults().add(result);
+
+        System.out.println("결과 : "+result);
 
 
-        while (true) {
+        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력시 삭제)");
+        if(Objects.equals(sc.next(),"remove")){
+            calculator.getResults().remove(index);
+        }
 
-            System.out.println("첫 번째 숫자를 입력하세요 : ");
-            int input1 = scanner.nextInt();
-
-            System.out.println("두 번째 숫자를 입력하세요 :");
-            int input2 = scanner.nextInt();
-
-            System.out.println("사칙연산 기호를 입력하세요 :");
-            String input3 = scanner.next();
-
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String input4 = scanner.next();
-
-
-            if (input4.equals("exit")) {
-                break;
+        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+        if(Objects.equals(sc.next(),"inquiry")){
+            for (Integer res : calculator.getResults()) {
+                System.out.println("Answers : "+ res);
             }
+        }
 
-            switch (input3) {
+        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료:)");
+        String str2= sc.next();
+        if(Objects.equals(str2, "exit")){
+            break;
+        }
 
-                case "+": {
-                    result = input1 + input2;
-                    break;
-                }
-
-                case "-": {
-                    result = input1 - input2;
-                    break;
-                }
-
-                case "*": {
-                    result = input1 * input2;
-                    break;
-                }
-
-                case "/": {
-                    result = input1 / input2;
-                    break;
-                }
-                default:
-                    System.out.println("올바른 접근이 아닙니다.");
-
-                    break;
-
-
-            }
-
-            System.out.println("결과 : " + result);
-            results.add(result);
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if (Objects.equals(scanner.next(), "remove")) {
-                results.remove(0);
-
-            }
-            System.out.println("결과 저장소 : " + results);
 
         }
+
+
     }
 }
---
+++ homework/Calculator.java
@@ -0,0 +1,50 @@
+package homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    private  List<Integer> results=new ArrayList<>();
+
+    public List<Integer> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Integer> results) {
+        this.results = results;
+    }
+
+    public int calculate(int num1, int num2,char operator) {
+
+     int result=0;
+
+     switch(operator){
+
+         case '+':
+             result= num1 + num2;
+             break;
+
+         case '-':
+             result= num1 - num2;
+             break;
+
+         case'*':
+             result =num1 * num2;
+             break;
+
+         case '/':
+             if(num2 !=0){
+                 result= num1 / num2;
+             }else{
+                 throw new ArithmeticException("나눗셈 연산에서 부모에 0이 입력될 수 없습니다.");
+             }
+
+         default:
+             throw new ArithmeticException("유효하지 않은 연산자입니다.");
+
+
+        }
+        return result;
+    }
+}
